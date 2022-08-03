f3848dd673bd8e28638bfd33106069c071e5ed2a
zolajigar <kingofdarkness012345@gmail.com>
2022-08-03 11:03:17 +0530

    030820221103

--- index.js
+++ index.js
@@ -10,8 +10,35 @@
 
 //primitives varibles data types - number , string , null , boolean , undefined 
 
-let age = 20 ; //Number (Integer type)
-let name = 'jigar' ; //String you can use single court or double court as you want
-let isApproved = false ; //Boolean type(True or false condition)
-let firstName = undefined ;  //Undefined value we will defined it later
-let lastName = null ; //Null type this varible will not contant anything now or in future
+// let age = 20 ; //Number (Integer type)
+// let name = 'jigar' ; //String you can use single court or double court as you want
+// let isApproved = false ; //Boolean type(True or false condition)
+// let firstName = undefined ;  //Undefined value we will defined it later
+// let lastName = null ; //Null type this varible will not contant anything now or in future
+
+
+let name = 'jigar';
+let age = 20 ;
+let person = {
+    name : 'jigar' , 
+    age : 20
+} ;
+
+console.log(person);
+
+
+//if you want to change the value of person object's property
+//There are 2 ways 
+
+// 1. Dot notation 
+person.name = 'Pratham';
+
+//Also use for reading value of property)
+console.log(person.name);
+
+
+// 2. Bracket notation  
+
+person['name'] = 'Aayush';
+console.log(person.name);
+
