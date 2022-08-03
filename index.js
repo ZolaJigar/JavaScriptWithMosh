fccea7e036d067ff1abdaa684d467cb6a7bc5279
zolajigar <kingofdarkness012345@gmail.com>
2022-08-03 10:40:09 +0530

    030820221040

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-  //its my first program of js
-  console.log(' Hello world' );
+//   //its my first program of js
+//   console.log(' Hello world' ); 
+//   let name = 'jigar';
+//   console.log(name);
+
+//   const intrestRate = 0.3 ;
+//   intrestRate = 3 ;
+//   console.log(intrestRate);
+
+
+//primitives varibles data types - number , string , null , boolean , undefined 
+
+let age = 20 ; //Number (Integer type)
+let name = 'jigar' ; //String you can use single court or double court as you want
+let isApproved = false ; //Boolean type(True or false condition)
+let firstName = undefined ;  //Undefined value we will defined it later
+let lastName = null ; //Null type this varible will not contant anything now or in future
